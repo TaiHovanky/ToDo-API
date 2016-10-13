ceec7efd1b9d1742d5d860edebaef73ef127ce25
TaiHovanky <tai.hovanky@gmail.com>
2016-10-13 10:14:28 -0500

    Refactor API with underscore

--- todoServer.js
+++ todoServer.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -21,23 +22,28 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(todo){
-        if(todo.id === todoId){
-            matchedTodo = todo;
-        } 
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId}); //searches through todo array for element that has an id equal to todoId
+    //todos.forEach(function(todo){
+        //if(todo.id === todoId){
+            //matchedTodo = todo;
+        //} 
+    //});  <--replaced by the underscore function _.findWhere
     if(matchedTodo){
         res.json(matchedTodo);
     } else {
-        res.status(404);
+        res.status(404).send();
     }
 
 });
 
 //POST will resemble get /todos. There's no :id since the id is generated after the POST
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed'); //use _.pick to remove extraneous data from req.body object
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    } //if the completed field isn't a Boolean or if the description isn't a string, return a 400 status
+    //the .trim removes the spaces and so if without its spaces, the entry's length is 0, we get the error
+    body.description = body.description.trim(); //remove whitespaces before and after the description string
     body.id = todoNextId++;
     todos.push(body);
     console.log('description ' + body.description);
