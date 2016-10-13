cc0191ff44b49ca599ab7f9977a4348713c90b98
TaiHovanky <tai.hovanky@gmail.com>
2016-10-13 09:39:47 -0500

    Add POST for todos

--- todoServer.js
+++ todoServer.js
@@ -1,22 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 5000;
 
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to Hyvee',
-    completed: false
-}, {
-    id: 3,
-    description: 'Take out trash',
-    completed: true
-}]; //the todo will be the model - individual unit that you'll have multiple of
+var todos = []; //the todo will be the model - individual unit that you'll have multiple of
 //will be using collection-model language a lot
+var todoNextId = 1;
+
+app.use(bodyParser.json()); //anytime a json request comes in, request parses it and we can access it via request.body
 
 app.get('/', function(req, res){
     res.send('Todo API root');
@@ -42,6 +34,16 @@ app.get('/todos/:id', function(req, res){
     }
 
 });
+
+//POST will resemble get /todos. There's no :id since the id is generated after the POST
+app.post('/todos', function(req, res){
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    console.log('description ' + body.description);
+    res.json(body);
+});
+
 
 app.listen(port, function(){
     console.log('express listening on port '+ port);
