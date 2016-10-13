6dd0d00d8c7a59ca058f04ec121ed4f59f83e131
TaiHovanky <tai.hovanky@gmail.com>
2016-10-13 11:34:58 -0500

    Add PUT todos

--- todoServer.js
+++ todoServer.js
@@ -61,6 +61,33 @@ app.delete('/todos/:id', function(req, res){
     }
 });
 
+//PUT/todos/:ID
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')){
+        //runs if property exists but isn't Boolean'
+        return res.status(400).send();
+    } 
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    } else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes); //don't need to explicitly update array - objects are passed by reference
+    res.json(matchedTodo);
+});
+
 
 app.listen(port, function(){
     console.log('express listening on port '+ port);
